39da26c24333f1612f035ccae6b35549738f5ca2
KriperoNio <75113283+KriperoNio@users.noreply.github.com>
2023-05-22 16:42:06 +0300

    DEBUGIN PAGER!!!

--- lib/pages/navigation_bar_tabs/MainPage.dart
+++ lib/pages/navigation_bar_tabs/MainPage.dart
@@ -3,26 +3,25 @@ import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class MainPage extends StatelessWidget {
-  const MainPage ({super.key});
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    return Stack(
-      children: [
-        Image(
-          image: const NetworkImage("https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/backgrounds%2Fyusuf-evli-bVq6bh26H-Y-unsplash.jpg?alt=media&token=770e26c4-a42a-4675-b978-5c34226a04fb"),
-          fit: BoxFit.cover,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-        ),
-        const Scaffold(
-          backgroundColor: Colors.transparent,
-          bottomNavigationBar: NavigationBarMain(),
-          body: VideosScreen(),
-        ),
-      ]
-    );
+    return Stack(children: [
+      Image(
+        image: const NetworkImage(
+            "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/backgrounds%2Fyusuf-evli-bVq6bh26H-Y-unsplash.jpg?alt=media&token=770e26c4-a42a-4675-b978-5c34226a04fb"),
+        fit: BoxFit.cover,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+      ),
+      const Scaffold(
+        backgroundColor: Colors.transparent,
+        bottomNavigationBar: NavigationBarMain(),
+        body: VideosScreen(),
+      ),
+    ]);
   }
 }
 
@@ -110,18 +109,21 @@ class NavigationBarMainState extends State<NavigationBarMain> {
   ];
 }
 
-final videos = ['assets/videos/Water_3.mp4', 'assets/videos/Black_Coffee.mp4', "assets/videos/Flower_3.mp4", "assets/videos/high_rise_skyscraper_flags_410.mp4"];
+final videos = [
+  'assets/videos/Water_3.mp4',
+  'assets/videos/Black_Coffee.mp4',
+  "assets/videos/Flower_3.mp4",
+  "assets/videos/high_rise_skyscraper_flags_410.mp4"
+];
 
 class VideosScreen extends StatefulWidget {
   const VideosScreen({super.key});
-
   @override
   State<VideosScreen> createState() => _VideosScreenState();
 }
 
 class _VideosScreenState extends State<VideosScreen> {
   late PageController _pageController;
-
   @override
   void initState() {
     super.initState();
@@ -181,7 +183,6 @@ class _VideoCardState extends State<VideoCard> {
   void dispose() {
     _controller.dispose();
     super.dispose();
-    _controller.dispose();
   }
 
   @override
